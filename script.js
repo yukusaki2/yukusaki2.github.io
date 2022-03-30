4e3fa1c984d7889a2f4774d5b65332378a591ba0
yukusaki2 <102636283+yukusaki2@users.noreply.github.com>
2022-03-30 18:42:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ $(function () {
 
     
     
-    $('#top-btn').click(function () {
+    $('#top-btn').click(function(){
         $('html, body').animate({
             'scrollTop': 0
         }, 500);
     });
 
-    $('header a').click(function () {
+    $('header a').click(function(){
         var id = $(this).attr('href');
         var position = $(id).offset().top;
         $('html, body').animate({
